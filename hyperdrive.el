13a8da8c232e8a6f7087a15966791374b5ed006d
Adam Porter <adam@alphapapa.net>
2023-04-18 15:27:51 -0500

    Meta: Require plz 0.5.4

--- hyperdrive.el
+++ hyperdrive.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Joseph Turner <joseph@ushin.org>
 ;; Created: 2022
 ;; Version: 0.0.1
-;; Package-Requires: ((emacs "27.1") (map "3.0") (compat "29.1.3.2") (plz "0.5") (persist "0.5"))
+;; Package-Requires: ((emacs "27.1") (map "3.0") (compat "29.1.3.2") (plz "0.5.4") (persist "0.5"))
 ;; Homepage: https://git.sr.ht/~ushin/hyperdrive.el
 
 ;; This program is free software; you can redistribute it and/or
