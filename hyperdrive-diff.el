ac8ac5be14161fedc69cfe83fc657307da862602
Joseph Turner <joseph@ushin.org>
2023-07-01 10:21:31 -0700

    Tidy: (hyperdrive-diff-mode) Move to beginning of next line
    (eq (1+ (pos-eol)) (pos-bol 2))  =>  t

--- hyperdrive-diff.el
+++ hyperdrive-diff.el
@@ -89,10 +89,10 @@ This function is intended to diff files, not directories."
   :interactive nil
   ;; Narrow the buffer to hide the diff command and "diff finished" lines.
   (narrow-to-region
-   (1+ (pos-eol)) (save-excursion
-                    (goto-char (point-max))
-                    (forward-line -2)
-                    (point))))
+   (pos-bol 2) (save-excursion
+                 (goto-char (point-max))
+                 (forward-line -2)
+                 (point))))
 
 ;;;; Footer
 
