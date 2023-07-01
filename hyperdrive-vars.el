c318bda327c2e3496660d05660fddc3891cfc15b
Adam Porter <adam@alphapapa.net>
2023-06-30 20:08:25 -0500

    Comment: Add TODO

--- hyperdrive-vars.el
+++ hyperdrive-vars.el
@@ -81,6 +81,7 @@ Passed to `format-time-string', which see."
   :set (lambda (option value)
          (set option value)
          (setf hyperdrive-timestamp-format-string
+               ;; TODO: Left-align this header.
                (format "%%%ds"
                        ;; FIXME: This value varies based on current
                        ;;        time. (format-time-string "%-I") will
