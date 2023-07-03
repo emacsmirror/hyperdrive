d1852176980b0e2440d2eb5684a060bc815c6f44
Joseph Turner <joseph@ushin.org>
2023-07-03 00:10:09 -0700

    Comment: Add TODO

--- hyperdrive-history.el
+++ hyperdrive-history.el
@@ -153,6 +153,7 @@ entry."
                          (hyperdrive-read-entry :force-prompt t :allow-version-p nil)
                        hyperdrive-current-entry)))
   ;; TODO: Highlight range for ENTRY
+  ;; TODO: What to do when ENTRY is a directory?
   (hyperdrive-fill-version-ranges entry :then
     (lambda ()
       (pcase-let* (((cl-struct hyperdrive-entry hyperdrive path) entry)
