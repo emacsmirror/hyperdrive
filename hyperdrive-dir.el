b0138ea127bf95641eaa21ec18f5d968f2916a39
Joseph Turner <joseph@ushin.org>
2023-07-02 23:49:46 -0700

    Fix: (hyperdrive-dir-mode) Make mode non-interactive

--- hyperdrive-dir.el
+++ hyperdrive-dir.el
@@ -103,6 +103,7 @@ With point on header, returns directory entry."
     ;; TODO: Add more to lighter, e.g. URL.
     )
   "Major mode for Hyperdrive directory buffers."
+  :interactive nil
   (setf hyperdrive-ewoc (ewoc-create #'hyperdrive-dir-pp)
         ;; TODO(alphapapa): Imenu support.
         ;; imenu-create-index-function #'ement-room--imenu-create-index-function
