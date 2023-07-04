d86d3c05126bdd89483e69227a21531f35c9d4bc
Joseph Turner <joseph@ushin.org>
2023-07-03 18:53:31 -0700

    Revert "Change: (hyperdrive-history-diff) Diff empty: message, don't pop buf"
    This reverts commit 6422913f1763d9e72cceb15c98ae049e0ffbfd9c.
    
    Just display message in diff buffer.

--- hyperdrive-history.el
+++ hyperdrive-history.el
@@ -233,7 +233,6 @@ entry."
           (goto-char (point-min)))))))
 
 (declare-function hyperdrive-diff-file-entries "hyperdrive-diff")
-(declare-function hyperdrive-diff-empty-diff-p "hyperdrive-diff")
 (defun hyperdrive-history-diff (old-entry new-entry)
   "Show diff between OLD-ENTRY and NEW-ENTRY.
 Interactively, diff range entry at point with previous entry."
@@ -250,12 +249,7 @@ Interactively, diff range entry at point with previous entry."
   ;; TODO: Only call `hyperdrive-diff-file-entries' when at least one entry exists.
   (hyperdrive-diff-file-entries old-entry new-entry
     :then (lambda (diff-buffer)
-            (if (with-current-buffer diff-buffer
-                  (hyperdrive-diff-empty-diff-p diff-buffer))
-                (hyperdrive-message "No difference between %s and %s"
-                                    (hyperdrive-entry-description old-entry)
-                                    (hyperdrive-entry-description new-entry))
-              (pop-to-buffer diff-buffer)))))
+            (pop-to-buffer diff-buffer))))
 
 (declare-function hyperdrive-open "hyperdrive")
 
