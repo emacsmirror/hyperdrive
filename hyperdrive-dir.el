bf967583860c785bfd99fb9bded9bcd1c3da6070
Adam Porter <adam@alphapapa.net>
2023-08-08 19:14:50 -0500

    Change: (hyperdrive-dir--imenu-create-index-function) Improvements

--- hyperdrive-dir.el
+++ hyperdrive-dir.el
@@ -186,8 +186,13 @@ Interactively, visit file or directory at point in
   "Return Imenu index for the current `hyperdrive-dir' buffer.
 For use as `imenu-create-index-function'."
   (cl-loop for node in (hyperdrive-ewoc-collect-nodes hyperdrive-ewoc #'identity)
-           collect (let ((location (goto-char (ewoc-location node))))
-                     (cons (buffer-substring (point) (pos-eol)) location))))
+           collect (let* ((location (goto-char (ewoc-location node)))
+                          (entry (ewoc-data node))
+                          (face (if (hyperdrive--entry-directory-p entry)
+                                    'hyperdrive-directory 'default)))
+                     (cons (propertize (hyperdrive-entry-name entry)
+                                       'face face)
+                           location))))
 
 (provide 'hyperdrive-dir)
 ;;; hyperdrive-dir.el ends here
