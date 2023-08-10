12707f6e8ba0ac914c433199b289414c1ceb495a
Joseph Turner <joseph@ushin.org>
2023-08-09 21:01:31 -0700

    Fix: (hyperdrive-handler-directory) Only call then when it exists

--- hyperdrive-handlers.el
+++ hyperdrive-handlers.el
@@ -166,7 +166,8 @@ arguments."
                 :else (lambda (_error) (message "ERROR"))))
             entries)
       (set-buffer-modified-p nil)
-      (funcall then))))
+      (when then
+        (funcall then)))))
 
 (cl-defun hyperdrive-handler-streamable (entry &key _then)
   ;; TODO: Is there any reason to not pass THEN through?
