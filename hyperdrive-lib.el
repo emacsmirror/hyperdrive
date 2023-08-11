fdeb091d096e3155bee2fd5b1021e621fcc2b7fd
Joseph Turner <joseph@ushin.org>
2023-08-11 12:03:55 -0700

    Tidy: (hyperdrive-api-default-else) Rename plz-error -> plz-err

--- hyperdrive-lib.el
+++ hyperdrive-lib.el
@@ -170,15 +170,15 @@ make the request."
        ;; We pass only the `plz-error' struct to the ELSE* function.
        (funcall else* (caddr err))))))
 
-(defun hyperdrive-api-default-else (else plz-error)
+(defun hyperdrive-api-default-else (else plz-err)
   "Handle common errors, overriding ELSE.
 Checks for common errors; if none are found, calls ELSE with
-PLZ-ERROR, if ELSE is non-nil; otherwise re-signals PLZ-ERROR.
-PLZ-ERROR should be a `plz-error' struct."
-  (cond ((equal 7 (car (plz-error-curl-error plz-error)))
+PLZ-ERR, if ELSE is non-nil; otherwise re-signals PLZ-ERR.
+PLZ-ERR should be a `plz-error' struct."
+  (cond ((equal 7 (car (plz-error-curl-error plz-err)))
          (hyperdrive-user-error "Gateway not running.  Use \"M-x hyperdrive-start RET\" to start it"))
-        (else (funcall else plz-error))
-        (t (signal 'plz-error (list "plz error" plz-error)))))
+        (else (funcall else plz-err))
+        (t (signal 'plz-error (list "plz error" plz-err)))))
 
 (defun hyperdrive--httpify-url (url)
   "Return localhost HTTP URL for HYPER-URL."
