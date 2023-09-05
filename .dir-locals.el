cf29756f661a5b86f04c9424ce7bdc3da3cc2e5e
Joseph Turner <joseph@ushin.org>
2023-09-05 02:20:15 -0700

    Chore: Add to ispell-buffer-session-localwords dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((ispell-buffer-session-localwords . ("dir" "hypercore" "hyperdrive" "hyperdrives" "hyperdrive's" "args" "systemd" "minibuffer" "petname" "petnames" "org" "plist" "plists" "alist" "alists" "existsp" "ETag" "streamable" "DNSLink" "ewoc" "struct" "ENTRY's" "localhost" "imenu")))))
+((nil . ((ispell-buffer-session-localwords . ("dir" "hypercore" "hyperdrive" "hyperdrives" "hyperdrive's" "args" "systemd" "minibuffer" "petname" "petnames" "org" "plist" "plists" "alist" "alists" "existsp" "ETag" "streamable" "DNSLink" "ewoc" "struct" "ENTRY's" "localhost" "imenu" "mtime" "accessor")))))
