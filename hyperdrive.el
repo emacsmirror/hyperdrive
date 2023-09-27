627edace7b799508dddf328df9c58009a9ec595d
Adam Porter <adam@alphapapa.net>
2023-09-27 16:40:13 -0500

    Tidy: Compiler warning about use-dialog-box-p
    use-dialog-box-p is in Emacs 29+.

--- hyperdrive.el
+++ hyperdrive.el
@@ -735,7 +735,9 @@ The return value of this function is the retrieval buffer."
              (?n "no" "exit without doing anything")
              (?s "save and then kill" "save the buffer and then kill it"))
            nil nil (and (not use-short-answers)
-                        (not (use-dialog-box-p)))))))
+                        (not (when (fboundp 'use-dialog-box-p)
+                               (with-no-warnings
+                                 (use-dialog-box-p)))))))))
     (if (equal response "no")
         nil
       (unless (equal response "yes")
