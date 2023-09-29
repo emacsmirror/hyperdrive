fcccde94a191da8b48e125bcc37fb38e8d7a03a2
Adam Porter <adam@alphapapa.net>
2023-09-29 14:57:02 -0500

    WIP

--- hyperdrive.el
+++ hyperdrive.el
@@ -771,7 +771,44 @@ The return value of this function is the retrieval buffer."
 (transient-define-prefix hyperdrive-menu ()
   ;; FIXME: Docstring.
   ""
-  [("?" "Info manual" hyperdrive-info-manual)]
+  [:class transient-row
+          :description (lambda ()
+                         (when-let ((hyperdrive-current-entry)
+                                    (hyperdrive (hyperdrive-entry-hyperdrive hyperdrive-current-entry)))
+                           (format "Public key:%s%s"
+                                   (hyperdrive--format-host
+                                    (hyperdrive-entry-hyperdrive hyperdrive-current-entry)
+                                    :format '(short-key))
+                                   (if (hyperdrive-writablep hyperdrive)
+                                       (format "  Seed:%s"
+                                               (hyperdrive--format-host
+                                                (hyperdrive-entry-hyperdrive hyperdrive-current-entry)
+                                                :format '(seed)))
+                                     ""))))
+          ;; public-key: 12fef3 seed: my-seed petname ("set" s p): Adam nickname: alphapapa
+          ("?" "Info manual" hyperdrive-info-manual)
+          ("s p" "Petname" hyperdrive-set-petname
+           :if (lambda () hyperdrive-current-entry)
+           :description (lambda ()
+                          (format "Petname: %s"
+                                  (pcase (hyperdrive-petname
+                                          (hyperdrive-entry-hyperdrive hyperdrive-current-entry))
+                                    (`nil (propertize "none"
+                                                      'face 'transient-inactive-value))
+                                    (it (propertize it
+                                                    'face 'transient-value))))))
+          ("s n" "Nickname" hyperdrive-set-nickname
+           :if (lambda () hyperdrive-current-entry)
+           :description (lambda ()
+                          (format "Nickname: %s"
+                                  ;; TODO: Hyperdrive-metadata accessor (and maybe gv setter).
+                                  (pcase (alist-get 'name
+                                                    (hyperdrive-metadata
+                                                     (hyperdrive-entry-hyperdrive hyperdrive-current-entry)))
+                                    (`nil (propertize "none"
+                                                      'face 'transient-inactive-value))
+                                    (it (propertize it
+                                                    'face 'transient-value))))))]
   [ ;; :class transient-subgroups
    ;; :pad-keys t
    ["Gateway"
