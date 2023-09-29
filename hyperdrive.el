87647b12ce1bfdc95f63f269f95ec6877252ae09
Adam Porter <adam@alphapapa.net>
2023-09-29 17:59:27 -0500

    Change: (hyperdrive-up) Take ENTRY as argument

--- hyperdrive.el
+++ hyperdrive.el
@@ -482,11 +482,11 @@ hyperdrive directory listing or a `hyperdrive-mode' file buffer."
     (kill-new url)
     (hyperdrive-message "%s" url)))
 
-(defun hyperdrive-up ()
-  "Go up to parent directory."
+(defun hyperdrive-up (entry)
+  "Go up to parent directory of ENTRY."
   (declare (modes hyperdrive-mode))
-  (interactive)
-  (if-let ((parent (hyperdrive-parent hyperdrive-current-entry)))
+  (interactive (list (hyperdrive--context-entry)))
+  (if-let ((parent (hyperdrive-parent entry)))
       ;; TODO: Go to entry in parent directory.
       (hyperdrive-open parent)
     (hyperdrive-user-error "At root directory")))
