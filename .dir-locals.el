6b1e063aa4049c724bd75b67ca0a99f624e4d428
Joseph Turner <joseph@ushin.org>
2023-10-20 17:05:11 -0700

    Meta: Add fill-column rule to .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((ispell-buffer-session-localwords . ("dir" "hypercore" "hyperdrive" "hyperdrives" "hyperdrive's" "args" "systemd" "minibuffer" "petname" "petnames" "org" "plist" "plists" "alist" "alists" "existsp" "ETag" "streamable" "DNSLink" "ewoc" "struct" "ENTRY's" "localhost" "imenu" "mtime" "accessor" "http" "prepended" "prepend" "hostname" "whitespace" "namespace" "filesystem" "hostnames" "subdirectories" "unsets")))))
+((nil . ((ispell-buffer-session-localwords . ("dir" "hypercore" "hyperdrive" "hyperdrives" "hyperdrive's" "args" "systemd" "minibuffer" "petname" "petnames" "org" "plist" "plists" "alist" "alists" "existsp" "ETag" "streamable" "DNSLink" "ewoc" "struct" "ENTRY's" "localhost" "imenu" "mtime" "accessor" "http" "prepended" "prepend" "hostname" "whitespace" "namespace" "filesystem" "hostnames" "subdirectories" "unsets"))))
+ (emacs-lisp-mode . ((fill-column . 80))))
