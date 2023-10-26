4c7fe06349e767a1980ac7f1a746a0b1b9fd3a8a
Adam Porter <adam@alphapapa.net>
2023-10-25 23:35:01 -0500

    Meta: (emacs-lisp-mode) Enable display-fill-column-indicator-mode
    Since we've agreed to aim for 80 columns, this will help.  Hopefully
    this isn't objectionable, since it shouldn't cause a warning when
    files are opened; but if anyone does, it can go in a .dir-locals-2.el
    file instead.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,5 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((ispell-buffer-session-localwords . ("dir" "hypercore" "hyperdrive" "hyperdrives" "hyperdrive's" "args" "systemd" "minibuffer" "petname" "petnames" "org" "plist" "plists" "alist" "alists" "existsp" "ETag" "streamable" "DNSLink" "ewoc" "struct" "ENTRY's" "localhost" "imenu" "mtime" "accessor" "http" "prepended" "prepend" "hostname" "whitespace" "namespace" "filesystem" "hostnames" "subdirectories" "unsets"))))
- (emacs-lisp-mode . ((fill-column . 80))))
+ (emacs-lisp-mode . ((eval . (display-fill-column-indicator-mode))
+                     (fill-column . 80))))
