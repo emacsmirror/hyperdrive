caa7b0d4df97aa5421e44f1eac6230fbbc299dd8
Adam Porter <adam@alphapapa.net>
2023-11-16 21:35:19 -0600

    Tidy

--- hyperdrive-vars.el
+++ hyperdrive-vars.el
@@ -241,7 +241,7 @@ Each metadata item may be one of:
 In each corresponding format string, \"%s\" is replaced with the
 metadatum. Used in `hyperdrive-buffer-name-format', which see."
   :type '(alist :key-type symbol :value-type string)
-  :options '(petname nickname version domains public-key short-key seed))
+  :options '(name version path petname nickname public-key short-key seed domains))
 
 ;;;;; Faces
 
