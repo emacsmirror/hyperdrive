29748d4fe959a159b577cc5c2e8eaf07d5942db0
Adam Porter <adam@alphapapa.net>
2024-01-10 23:05:51 -0600

    WIP

--- sophia.el
+++ sophia.el
@@ -64,14 +64,14 @@
     dest))
 
 (cl-defun sophia-filter
-    (source value relations 
-            &key (limit 5) (hops 1) (predicate #'>=)
-            (modifier-fns (list (lambda (a b hops)
-                                  (setf (plist-get (cdr b) :hops) hops)
-                                  b)
-                                #'sophia-decay)))
+    (name value relations 
+          &key (limit 5) (hops 1) (predicate #'>=)
+          (modifier-fns (list (lambda (a b hops)
+                                (setf (plist-get (cdr b) :hops) hops)
+                                b)
+                              #'sophia-decay)))
   "Return list of destinations trusted by SOURCE by at least MIN."
-  (let* ((destinations (map-elt relations source)))
+  (let* ((destinations (map-elt relations name)))
     (when modifier-fns
       (let ((source (cl-typecase source
                       (string (list source :weight 1.0))
@@ -94,9 +94,83 @@
 (sophia-test
  (sophia-filter "alice" 0.2 (map-elt sophia-topics "tofu")))
 
-;; (("carole" :hops 1 :weight 0.8)
-;;  ("bob" :hops 1 :weight 0.25)
-;;  (("eve" :hops 2 :weight 0.4)
-;;   ("david" :hops 2 :weight 0.6400000000000001)
-;;   (("eve" :hops 3 :weight 0.5120000000000001))))
+;;;;
+
+(cl-defun sophia-filter (source-name relations &key (source-weight 1.0) (predicate #'>=) (weight-limit 0.2) (hops 1) (hop-limit 5)
+                                     (modifiers (list (lambda (a b hops)
+                                                        (setf (plist-get (cdr b) :hops) hops)
+                                                        b)
+                                                      #'sophia-decay)))
+  (let* ((destinations (map-elt relations source-name)))
+    (when modifiers
+      (dolist (modifier modifiers)
+        (setf destinations (mapcar (lambda (destination)
+                                     (funcall modifier source destination hops))
+                                   destinations))))
+    (setf destinations (map-filter (pcase-lambda (destination (map :weight))
+                                     (funcall predicate weight weight-limit))
+                                   destinations))
+    (remq nil
+          (append destinations
+                  (unless (>= hops hop-limit )
+                    (mapcar (pcase-lambda (`(,source-name . ,(map :source-weight)))
+                              (sophia-filter source-name (map-elt destinations source-naem)
+                                             :source-weight source-weight :predicate predicate :weight-limit weight-limit
+                                             :hops (1+ hops) :hop-limit hop-limit))
+                            destinations))))))
+
+(sophia-test
+ (sophia-filter "alice" (map-elt sophia-topics "tofu")))
+
+;;;;
+
+(cl-defstruct sophia-relation
+  from to weight)
+
+(defvar sophia-relations (make-hash-table :test #'equal))
+
+(defun sophia-add-relation (from to weight topic table)
+  (let ((relation (make-sophia-relation :from from :to to :weight weight)))
+    (push relation (map-elt (map-elt table topic) (sophia-relation-from relation)))))
+
+(defmacro sophia-test (&rest body)
+  `(progn
+     (clrhash sophia-relations)
+     (sophia-add-relation "alice" "bob" 0.25 "tofu" sophia-relations)
+     (sophia-add-relation "alice" "carole" 0.8 "tofu" sophia-relations)
+     (sophia-add-relation "carole" "david" 0.8 "tofu" sophia-relations)
+     (sophia-add-relation "carole" "eve" 0.5 "tofu" sophia-relations)
+     (sophia-add-relation "david" "eve" 0.8 "tofu" sophia-relations)
+     ,@body))
+
+(sophia-test
+ sophia-relations)
+
+(cl-defun sophia-filter (relations &key topic from predicate (hops 1))
+  (let* ((topic-relations (map-elt relations topic))
+         (relations-from (map-elt topic-relations from))
+         (valid-relations (cl-remove-if-not (pcase-lambda ((cl-struct sophia-relation from to weight))
+                                              (funcall predicate :from from :to to :weight weight :hops hops))
+                                            relations-from)))
+    (remq nil
+          (append valid-relations
+                  (mapcar (lambda (relation)
+                            (sophia-filter relations :topic topic :from (sophia-relation-to relation)
+                                           :predicate predicate :hops (1+ hops)))
+                          valid-relations)))))
+
+(sophia-test
+ (sophia-filter sophia-relations
+                :topic "tofu"
+                :from "alice"
+                :predicate (cl-function
+                            (lambda (&key from to weight hops)
+                              (and (<= hops 5)
+                                   (>= weight 0.2)))) ))
+(#s(sophia-relation "alice" "carole" 0.8)
+ #s(sophia-relation "alice" "bob" 0.25)
+ (#s(sophia-relation "carole" "eve" 0.5)
+  #s(sophia-relation "carole" "david" 0.8)
+  (#s(sophia-relation "david" "eve" 0.8))))
+
 
