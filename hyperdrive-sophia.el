d6f95722fa2f9b64c8a60d352c4462ac6c6c29fc
Adam Porter <adam@alphapapa.net>
2024-01-11 22:46:09 -0600

    WIP

--- hyperdrive-sophia.el
+++ hyperdrive-sophia.el
@@ -1,5 +1,3 @@
-(defvar sophia-relations (make-hash-table :test #'equal))
-
 (cl-defstruct sophia-relation
   from to score)
 
@@ -7,6 +5,12 @@
 
 (defun sophia-add-relation (from to score topic table)
   (let ((relation (make-sophia-relation :from from :to to :score score)))
-    (push relation (map-elt (map-elt table topic) (sophia-relation-from relation)))))
+    (push relation (map-elt (map-elt table
+                                     (sophia-relation-from relation))
+                            topic))))
+
+(cl-defun sophia-paths (from topic relations &key (max-hops 3))
+  "Return paths from FROM up to MAX-HOPS in RELATIONS about TOPIC."
+  )
 
 (provide 'hyperdrive-sophia)
--- tests/test-hyperdrive-sophia.el
+++ tests/test-hyperdrive-sophia.el
@@ -11,14 +11,59 @@
      (sophia-add-relation "carole" "david" 0.8 "tofu" sophia-relations)
      (sophia-add-relation "carole" "eve" 0.5 "tofu" sophia-relations)
      (sophia-add-relation "david" "eve" 0.8 "tofu" sophia-relations)
-     ,@body))
+     (cl-labels ((sophia-relations-for (peer)
+                   (cdar (map-filter (lambda (from data)
+                                       (equal from peer))
+                                     sophia-relations))))
+       ,@body)))
+
+(sophia-test
+  (sophia-relations-for "alice"))
+(("tofu" #s(sophia-relation "alice" "carole" 0.8)
+  #s(sophia-relation "alice" "bob" 0.25)))
+
+
+
+
+
+(ert-deftest sophia-paths-alice-tofu-2-hop ()
+  "Return alice's paths for \"tofu\" up to 2 hops away."
+  (sophia-test 
+    (let* ((from-alice (sophia-relations-for "alice"))
+           (about-tofu (map-elt from-alice "tofu"))
+           (1-hop-paths (cl-loop for relation in from-alice
+                                 collect (make-sophia-path
+                                          :score (sophia-relation-score relation)
+                                          :relations (list relation))))
+           (2-hop-paths (cl-loop for relation in from-alice
+                                 collect (make-sophia-path
+                                          :score (sophia-relation-score relation)
+                                          :relations (list relation))))
+           (paths (append 1-hop-paths 2-hop-paths)))
+      (should
+       (seq-set-equal-p
+        paths
+        (list (make-sophia-path
+               :score 0.25
+               :relations (list (make-sophia-relation :from "alice" :to "bob" :score 0.25)))
+              (make-sophia-path
+               :score 0.8
+               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)))
+              (make-sophia-path
+               :score 0.64
+               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)
+                                (make-sophia-relation :from "carole" :to "david" :score 0.8)))
+              (make-sophia-path
+               :score 0.4
+               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)
+                                (make-sophia-relation :from "carole" :to "eve" :score 0.5)))))))))
 
 (ert-deftest sophia-paths-alice-tofu-1-hop ()
   "Return alice's paths for \"tofu\" up to 1 hop away."
   (sophia-test 
-    (let* ((about-tofu (map-elt sophia-relations "tofu"))
-           (from-alice (map-elt about-tofu "alice"))
-           (paths (cl-loop for relation in from-alice
+    (let* ((from-alice (sophia-relations-for "alice"))
+           (about-tofu (map-elt from-alice "tofu"))
+           (paths (cl-loop for relation in about-tofu
                            collect (make-sophia-path
                                     :score (sophia-relation-score relation)
                                     :relations (list relation)))))
