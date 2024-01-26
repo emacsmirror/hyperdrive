b2c1d4db335602b6eddb7b0e183e42300571c8ba
Adam Porter <adam@alphapapa.net>
2024-01-25 20:28:46 -0600

    Rename tests

--- tests/test-hyperdrive-sophia.el
+++ tests/test-hyperdrive-sophia.el
@@ -3,82 +3,82 @@
 (require 'map)
 (require 'seq)
 
-(require 'hyperdrive-sophia)
+(require 'hyperdrive-fons)
 
-(defvar test-hyperdrive-sophia-relations (make-hash-table :test 'equal))
+(defvar test-hyperdrive-fons-relations (make-hash-table :test 'equal))
 
-(defvar test-hyperdrive-sophia-default-relations-fn
+(defvar test-hyperdrive-fons-default-relations-fn
   (lambda ()
-    (sophia-add-relation "alice" "bob" 0.25 "tofu" test-hyperdrive-sophia-relations)
-    (sophia-add-relation "alice" "carole" 0.8 "tofu" test-hyperdrive-sophia-relations)
-    (sophia-add-relation "carole" "david" 0.8 "tofu" test-hyperdrive-sophia-relations)
-    (sophia-add-relation "carole" "eve" 0.5 "tofu" test-hyperdrive-sophia-relations)
-    (sophia-add-relation "david" "eve" 0.8 "tofu" test-hyperdrive-sophia-relations)))
+    (fons-add-relation "alice" "bob" 0.25 "tofu" test-hyperdrive-fons-relations)
+    (fons-add-relation "alice" "carole" 0.8 "tofu" test-hyperdrive-fons-relations)
+    (fons-add-relation "carole" "david" 0.8 "tofu" test-hyperdrive-fons-relations)
+    (fons-add-relation "carole" "eve" 0.5 "tofu" test-hyperdrive-fons-relations)
+    (fons-add-relation "david" "eve" 0.8 "tofu" test-hyperdrive-fons-relations)))
 
-(cl-defmacro sophia-test ((&optional relations-fn) &rest body)
+(cl-defmacro fons-test ((&optional relations-fn) &rest body)
   (declare (indent defun) (debug (def-form)))
   `(progn
-     (clrhash test-hyperdrive-sophia-relations)
+     (clrhash test-hyperdrive-fons-relations)
      (funcall ,(if relations-fn
                    relations-fn
-                 test-hyperdrive-sophia-default-relations-fn))
-     (cl-letf (((symbol-function 'sophia-relations)
+                 test-hyperdrive-fons-default-relations-fn))
+     (cl-letf (((symbol-function 'fons-relations)
                 (lambda (peer-name)
                   (cdar (map-filter (lambda (from data)
                                       (equal from peer-name))
-                                    test-hyperdrive-sophia-relations)))))
+                                    test-hyperdrive-fons-relations)))))
        ,@body)))
 
-(ert-deftest sophia-paths-alice-tofu-3-hops ()
+(ert-deftest fons-paths-alice-tofu-3-hops ()
   "Return alice's paths for \"tofu\" up to 3 hops away."
-  (sophia-test ()
-    (let* ((paths (sophia-paths "alice" "tofu" :max-hops 3)))
+  (fons-test ()
+    (let* ((paths (fons-paths "alice" "tofu" :max-hops 3)))
       (should
        (seq-set-equal-p
         paths
-        (list (make-sophia-path
+        (list (make-fons-path
                :score 0.25
-               :relations (list (make-sophia-relation :from "alice" :to "bob" :score 0.25)))
-              (make-sophia-path
+               :relations (list (make-fons-relation :from "alice" :to "bob" :score 0.25)))
+              (make-fons-path
                :score 0.8
-               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)))
-              (make-sophia-path
+               :relations (list (make-fons-relation :from "alice" :to "carole" :score 0.8)))
+              (make-fons-path
                :score 0.6400000000000001
-               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)
-                                (make-sophia-relation :from "carole" :to "david" :score 0.8)))
-              (make-sophia-path
+               :relations (list (make-fons-relation :from "alice" :to "carole" :score 0.8)
+                                (make-fons-relation :from "carole" :to "david" :score 0.8)))
+              (make-fons-path
                :score 0.4
-               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)
-                                (make-sophia-relation :from "carole" :to "eve" :score 0.5)))
-              (make-sophia-path
+               :relations (list (make-fons-relation :from "alice" :to "carole" :score 0.8)
+                                (make-fons-relation :from "carole" :to "eve" :score 0.5)))
+              (make-fons-path
                :score 0.5120000000000001
-               :relations (list (make-sophia-relation :from "alice" :to "carole" :score 0.8)
-                                (make-sophia-relation :from "carole" :to "david" :score 0.8)
-                                (make-sophia-relation :from "david" :to "eve" :score 0.8)))))))))
+               :relations (list (make-fons-relation :from "alice" :to "carole" :score 0.8)
+                                (make-fons-relation :from "carole" :to "david" :score 0.8)
+                                (make-fons-relation :from "david" :to "eve" :score 0.8)))))))))
 
-(ert-deftest sophia-filter-short-circuits ()
+(ert-deftest fons-filter-short-circuits ()
   ;; NEXT: Write this test to ensure that the paths from alice to georgie and to
   ;; hobart are filtered out.
-  (sophia-test ((lambda ()
-                  (funcall test-hyperdrive-sophia-default-relations-fn)
-                  (sophia-add-relation "alice" "frank" 1 "tofu" test-hyperdrive-sophia-relations)
-                  (sophia-add-relation "frank" "georgie" 0.2 "tofu" test-hyperdrive-sophia-relations)
-                  (sophia-add-relation "georgie" "hobart" 0.8 "tofu" test-hyperdrive-sophia-relations)))
-    (let (sophia-relations-called-with)
-      (cl-letf* ((orig-fn (symbol-function 'sophia-relations))
-                 ((symbol-function 'sophia-relations)
+  (fons-test ((lambda ()
+                  (funcall test-hyperdrive-fons-default-relations-fn)
+                  (fons-add-relation "alice" "frank" 1 "tofu" test-hyperdrive-fons-relations)
+                  (fons-add-relation "frank" "georgie" 0.2 "tofu" test-hyperdrive-fons-relations)
+                  (fons-add-relation "georgie" "hobart" 0.8 "tofu" test-hyperdrive-fons-relations)))
+    (let (fons-relations-called-with)
+      (cl-letf* ((orig-fn (symbol-function 'fons-relations))
+                 ((symbol-function 'fons-relations)
                   (lambda (&rest args)
-                    (push args sophia-relations-called-with)
+                    (push args fons-relations-called-with)
                     (apply orig-fn args))))
-        (sophia-paths "alice" "tofu" :max-hops 3)
-        (should (seq-contains-p sophia-relations-called-with '("frank")))
-        (should-not (seq-contains-p sophia-relations-called-with '("georgie")))))))
+        (fons-paths "alice" "tofu" :max-hops 3)
+        (should (seq-contains-p fons-relations-called-with '("frank")))
+        (should-not (seq-contains-p fons-relations-called-with '("georgie")))))))
 
 ;; Local Variables:
 ;; read-symbol-shorthands: (
 ;;   ("he//" . "hyperdrive-entry--")
 ;;   ("he/"  . "hyperdrive-entry-")
 ;;   ("h//"  . "hyperdrive--")
-;;   ("hs/"  . "hyperdrive-sophia-")
+;;   ("hf/"  . "hyperdrive-fons-")
 ;;   ("h/"   . "hyperdrive-"))
 ;; End:
