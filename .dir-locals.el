024d74b64f992f7c3516cda3c5c37ce7d12fc65a
Adam Porter <adam@alphapapa.net>
2024-05-18 19:02:28 -0500

    Meta: (.dir-locals.el) Set org-id-link-to-org-use-id nil

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,5 @@
 
 ((nil . ((ispell-buffer-session-localwords . ("dir" "hypercore" "hyperdrive" "hyperdrives" "hyperdrive's" "args" "systemd" "minibuffer" "petname" "petnames" "org" "plist" "plists" "alist" "alists" "existsp" "ETag" "streamable" "DNSLink" "ewoc" "struct" "ENTRY's" "localhost" "imenu" "mtime" "accessor" "http" "prepended" "prepend" "hostname" "whitespace" "namespace" "filesystem" "hostnames" "subdirectories" "unsets" "finalizer" "subprocess" "autodetect" "transclusion"))))
  (emacs-lisp-mode . ((eval . (display-fill-column-indicator-mode))
-                     (fill-column . 80))))
+                     (fill-column . 80)))
+ (org-mode . ((org-id-link-to-org-use-id . nil))))
