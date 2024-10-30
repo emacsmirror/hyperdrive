bb1d6c8d0b2fb708f3589c61b88a148566d50bf5
Joseph Turner <joseph@ushin.org>
2024-10-30 01:37:47 -0700

    Tidy: (h/fons-view--format-graph) Consolidate root logic

--- hyperdrive-fons-view.el
+++ hyperdrive-fons-view.el
@@ -247,7 +247,8 @@ called and replaces the buffer content with the rendered output."
                          root (funcall label-fun root) root "purple"
                          hyperdrive-fons-view-source-color
                          hyperdrive-fons-view-blocker-color))
-                (insert (format "{ rank=\"source\"; %s; }" root))))
+                (insert (format "{ rank=\"source\"; %s; }" root))
+                (insert (format "root=\"%s\"" root-name))))
     (with-temp-buffer
       (save-excursion
         (insert "digraph fonsrelationview {\n")
@@ -279,8 +280,6 @@ called and replaces the buffer content with the rendered output."
                      hop hyperdrive-fons-view-blocked-color))))
         (format-root root-name)
         (maphash #'format-relation-to merge-relations)
-        (when root-name
-          (insert (format "root=\"%s\"" root-name)))
         (insert "}"))
       (buffer-string))))
 
