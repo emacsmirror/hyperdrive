ba470fee8bda9cebaba9ad71a735a911e992a8ab
Joseph Turner <joseph@ushin.org>
2024-11-01 19:56:20 -0700

    Tidy: (h/menu-peer-graph) Remove unnecessary require and defvar

--- hyperdrive-menu.el
+++ hyperdrive-menu.el
@@ -440,11 +440,9 @@
   (interactive)
   (h/describe-hyperdrive (h/menu--scope)))
 
-(defvar h/peer-graph-menu)
 (transient-define-suffix h/menu-peer-graph ()
   :description "Peer graph"
   (interactive)
-  (require 'hyperdrive-peer-graph)
   (h/peer-graph-menu h/peer-graph-topic (h/menu--scope)))
 
 (transient-define-suffix h/menu-hyperdrive-copy-url ()
