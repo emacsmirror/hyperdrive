94e8ee1876c209bc8f5da37a0764440bdd5f786b
Joseph Turner <joseph@ushin.org>
2024-11-06 14:10:37 -0800

    Tidy: Reorganize hyperdrive-peer-graph.el

--- hyperdrive-peer-graph.el
+++ hyperdrive-peer-graph.el
@@ -33,8 +33,44 @@
   "Customization of the hyperdrive peer graph."
   :group 'hyperdrive)
 
+(defcustom hpg/default-topic "_default"
+  "Special topic name used as a fallback when no topic is specified."
+  :type 'string)
+
+(defcustom hpg/sources-max-hops-default 3
+  "Default number of hops to jump for sources."
+  :type 'integer)
+
+(defcustom hpg/blockers-max-hops-default 3
+  "Default number of hops to jump for blockers."
+  :type 'integer)
+
+(defcustom hpg/buffer-name "*hyperdrive-peer-graph-view*"
+  "Buffer name to show peer graph."
+  :type 'string)
+
+(defcustom hpg/display-buffer-action '(display-buffer-reuse-window)
+  "Display buffer action for hyperdrive peer graph.
+Passed to `display-buffer', which see."
+  :type display-buffer--action-custom-type)
+
 (defconst hpg/data-filename "/.well-known/peer-graph.json"
   "Hyperdrive filename to search for peer graph data.")
+
+(defvar hpg/root-hyperdrive nil)
+(defvar hpg/topic nil)
+(defvar hpg/relations nil)
+
+(defvar hpg/sources-max-hops hpg/sources-max-hops-default)
+(defvar hpg/blockers-max-hops hpg/blockers-max-hops-default)
+
+(defvar hpg/show-sources-p t)
+(defvar hpg/show-blockers-p t)
+(defvar hpg/show-blocked-p t)
+(defvar hpg/show-all-blocked-p nil)
+
+(defvar hpg/shortest-path-p t)
+(defvar hpg/paths-only-to nil)
 
 ;;;; Functions:
 
@@ -94,7 +130,171 @@ Call THEN with a list of block IDs."
     (format "<%s<br/><FONT POINT-SIZE=\"10\">%s</FONT>>"
             (h//format-preferred hyperdrive) (h//preferred-format hyperdrive))))
 
-;;;; Graph minor mode
+(cl-defun hpg/relations (root topic &key finally sources-max-hops blockers-max-hops)
+  "Load relations from ROOT about TOPIC and call FINALLY.
+FINALLY should be a function which accepts a single argument, a
+hash table of `fons-relation' structs keyed by public key.
+SOURCES-MAX-HOPS and BLOCKERS-MAX-HOPS are the maximum number of
+hops to traverse for sources and blockers, respectively."
+  (fons-relations
+   root :hops-fn #'hpg/hops-fn :topic "_blockers" :type 'blockers
+   :max-hops blockers-max-hops
+   :finally
+   (lambda (relations)
+     (fons-blocked
+      relations :blocked-fn #'hpg/blocked-fn :finally
+      (lambda (relations)
+        (fons-relations
+         root :relations relations :hops-fn #'hpg/hops-fn :topic topic
+         :type 'sources :max-hops sources-max-hops :finally
+         (lambda (relations)
+           (funcall finally relations))))))))
+
+(defun hpg/filter (relations)
+  "Return filtered RELATIONS."
+  ;; TODO: Make filters customizable
+  (unless (and hpg/show-sources-p hpg/show-blockers-p hpg/show-blocked-p hpg/show-all-blocked-p)
+    (cl-callf fons-filter-to-types relations
+      :sourcesp hpg/show-sources-p
+      :blockersp hpg/show-blockers-p
+      :blockedp hpg/show-blocked-p
+      :all-blocked-p (and hpg/show-blocked-p hpg/show-all-blocked-p)))
+  (when hpg/shortest-path-p
+    (cl-callf fons-filter-shortest-path relations))
+  ;; Apply `hpg/paths-only-to' last
+  (cl-callf2 fons-filter-paths-only-to
+      (mapcar #'h/public-key hpg/paths-only-to) relations)
+  relations)
+
+;;;;; Reading user input
+
+(defvar hpg/topic-history nil
+  "Minibuffer history of `hyperdrive-peer-graph-read-topic'.")
+
+(defun hpg/read-topic ()
+  "Read topic string or nil with blank string."
+  (let ((topic (read-string "Topic (leave blank for default topic): "
+                            nil hpg/topic-history)))
+    (and (not (string-blank-p topic))
+         topic)))
+
+(cl-defun hpg/context-topic (&key force-prompt)
+  "Return `hyperdrive-peer-graph-topic'.
+With FORCE-PROMPT, or interactively with universal prefix
+argument \\[universal-argument], always prompt.
+Blank string defaults to `hyperdrive-peer-graph-default-topic'."
+  (if force-prompt (hpg/read-topic) hpg/topic))
+
+(cl-defun hpg/context-root-hyperdrive (&key force-prompt)
+  "Return `hyperdrive-peer-graph-root-hyperdrive' or prompt for drive.
+With FORCE-PROMPT, or interactively with universal prefix
+argument \\[universal-argument], always prompt."
+  (if (and hpg/root-hyperdrive (not force-prompt))
+      hpg/root-hyperdrive
+    (h/read-hyperdrive :default (or (and h/current-entry
+                                         (he/hyperdrive h/current-entry))
+                                    hpg/root-hyperdrive))))
+
+(defvar hpg/max-hops-history nil
+  "Minibuffer history of `hyperdrive-peer-graph-read-max-hops'.")
+
+(defun hpg/read-max-hops (type)
+  "Read max hops for \\+`sources' or \\+`blockers' TYPE."
+  (let ((default (pcase type
+                   ('sources hpg/sources-max-hops)
+                   ('blockers hpg/blockers-max-hops))))
+    (read-number (format "Max hops for %s" type) default
+                 hpg/max-hops-history)))
+
+(defun hpg/context-max-hops (type &key force-prompt)
+  "Return `hyperdrive-peer-graph-sources-max-hops' or prompt.
+With FORCE-PROMPT, or interactively with universal prefix
+argument \\[universal-argument], always prompt."
+  (if force-prompt
+      (hpg/read-max-hops type)
+    (pcase type
+      ('sources hpg/sources-max-hops)
+      ('blockers hpg/blockers-max-hops))))
+
+(defun hpg/interactive-args ()
+  "Return list of interactive args for `hyperdrive-peer-graph'."
+  (let* ((root-hyperdrive (hpg/context-root-hyperdrive
+                           :force-prompt current-prefix-arg))
+         (topic (hpg/context-topic :force-prompt current-prefix-arg))
+         (topic-or-root-changed
+          (not (and (eq topic hpg/topic)
+                    (eq root-hyperdrive hpg/root-hyperdrive))))
+         (sources-max-hops (hpg/context-max-hops
+                            'sources :force-prompt (or current-prefix-arg
+                                                       topic-or-root-changed)))
+         (blockers-max-hops (hpg/context-max-hops
+                             'blockers
+                             :force-prompt (or current-prefix-arg
+                                               topic-or-root-changed))))
+    (list topic root-hyperdrive sources-max-hops blockers-max-hops)))
+
+;;;; Peer Graph
+
+(defun hyperdrive-peer-graph
+    (topic hyperdrive sources-max-hops blockers-max-hops)
+  "Show menu for HYPERDRIVE peer graph."
+  (interactive (hpg/interactive-args))
+  (if (and (equal topic hpg/topic)
+           hpg/root-hyperdrive
+           (h/equal-p hyperdrive hpg/root-hyperdrive)
+           (hpg/loaded-relations))
+      (hpg/display-graph)
+    (setf hpg/topic topic)
+    (setf hpg/root-hyperdrive hyperdrive)
+    (setf hpg/sources-max-hops sources-max-hops)
+    (setf hpg/blockers-max-hops blockers-max-hops)
+    (hpg/load)))
+
+(defun hpg/load ()
+  "Load `hpg/relations' and redisplay graph."
+  ;; TODO: Refill name and color metadata.
+  ;; TODO: If called in rapid succession, stop the requests from the first call.
+  (setf hpg/relations
+        (hpg/relations
+         (h/public-key hpg/root-hyperdrive)
+         (or hpg/topic hpg/default-topic)
+         :sources-max-hops hpg/sources-max-hops
+         :blockers-max-hops hpg/blockers-max-hops
+         :finally
+         (lambda (relations)
+           (setf hpg/relations relations)
+           (h/fill-metadata-all
+            (cons hpg/root-hyperdrive
+                  (mapcar #'h/url-hyperdrive (hash-table-keys hpg/relations)))
+            :finally (lambda ()
+                       (hpg/display-graph)
+                       (hpg/refresh-menu))))))
+  (hpg/display-loading-buffer))
+
+(defun hpg/display-loading-buffer ()
+  "Open loading buffer for hyperdrive peer graph."
+  (with-current-buffer (get-buffer-create hpg/buffer-name)
+    (with-silent-modifications
+      (erase-buffer)
+      (insert "Loading hyperdrive peer graph data...")
+      (display-buffer (current-buffer) hpg/display-buffer-action))))
+
+(defun hpg/display-graph ()
+  "Open buffer displaying hyperdrive peer graph."
+  (with-current-buffer (get-buffer-create hpg/buffer-name)
+    (h/fons-view (hpg/filter hpg/relations)
+                 (h/public-key hpg/root-hyperdrive)
+                 :focus-ids (mapcar #'h/public-key hpg/paths-only-to)
+                 :label-fun #'hpg/label-fun)
+    (hpg/mode)
+    (pop-to-buffer (current-buffer) hpg/display-buffer-action)))
+
+(defun hpg/loaded-relations ()
+  "Return `hyperdrive-peer-graph-relations' if loaded."
+  (and (not (processp hpg/relations))
+       hpg/relations))
+
+;;;; Minor mode
 
 (defun hpg/revert-buffer (&optional _ignore-auto _noconfirm)
   "Revert `hyperdrive-describe-mode' buffer.
@@ -118,7 +318,7 @@ Reload data and redisplay graph."
   :interactive nil
   (setq-local revert-buffer-function #'hpg/revert-buffer))
 
-;;;; Commands
+;;;;; Graph commands
 
 (defun hpg/view-follow-link (event)
   "Follow link at EVENT's position."
@@ -144,74 +344,6 @@ Reload data and redisplay graph."
              (h/open (he//create :hyperdrive ,hyperdrive :path "/")))]))))))
 
 ;;;; Transient UI
-
-(defvar hpg/root-hyperdrive nil)
-(defvar hpg/topic nil)
-(defvar hpg/relations nil)
-
-(defcustom hpg/sources-max-hops-default 3
-  "Default number of hops to jump for sources."
-  :type 'integer)
-
-(defcustom hpg/blockers-max-hops-default 3
-  "Default number of hops to jump for blockers."
-  :type 'integer)
-
-(defvar hpg/sources-max-hops hpg/sources-max-hops-default)
-(defvar hpg/blockers-max-hops hpg/blockers-max-hops-default)
-
-(defvar hpg/show-sources-p t)
-(defvar hpg/show-blockers-p t)
-(defvar hpg/show-blocked-p t)
-(defvar hpg/show-all-blocked-p nil)
-
-(defvar hpg/shortest-path-p t)
-(defvar hpg/paths-only-to nil)
-
-(defcustom hpg/buffer-name "*hyperdrive-peer-graph-view*"
-  "Buffer name to show peer graph."
-  :type 'string)
-
-(defcustom hpg/default-topic "_default"
-  "Special topic name used as a fallback when no topic is specified."
-  :type 'string)
-
-(defcustom hpg/display-buffer-action '(display-buffer-reuse-window)
-  "Display buffer action for hyperdrive peer graph.
-Passed to `display-buffer', which see."
-  :type display-buffer--action-custom-type)
-
-(defun hyperdrive-peer-graph
-    (topic hyperdrive sources-max-hops blockers-max-hops)
-  "Show menu for HYPERDRIVE peer graph."
-  (interactive (hpg/interactive-args))
-  (if (and (equal topic hpg/topic)
-           hpg/root-hyperdrive
-           (h/equal-p hyperdrive hpg/root-hyperdrive)
-           (hpg/loaded-relations))
-      (hpg/display-graph)
-    (setf hpg/topic topic)
-    (setf hpg/root-hyperdrive hyperdrive)
-    (setf hpg/sources-max-hops sources-max-hops)
-    (setf hpg/blockers-max-hops blockers-max-hops)
-    (hpg/load)))
-
-(defun hpg/interactive-args ()
-  "Return list of interactive args for `hyperdrive-peer-graph'."
-  (let* ((root-hyperdrive (hpg/context-root-hyperdrive
-                           :force-prompt current-prefix-arg))
-         (topic (hpg/context-topic :force-prompt current-prefix-arg))
-         (topic-or-root-changed
-          (not (and (eq topic hpg/topic)
-                    (eq root-hyperdrive hpg/root-hyperdrive))))
-         (sources-max-hops (hpg/context-max-hops
-                            'sources :force-prompt (or current-prefix-arg
-                                                       topic-or-root-changed)))
-         (blockers-max-hops (hpg/context-max-hops
-                             'blockers
-                             :force-prompt (or current-prefix-arg
-                                               topic-or-root-changed))))
-    (list topic root-hyperdrive sources-max-hops blockers-max-hops)))
 
 ;;;###autoload (autoload 'hyperdrive-peer-graph-menu "hyperdrive-peer-graph" nil t)
 (transient-define-prefix hyperdrive-peer-graph-menu
@@ -244,27 +376,6 @@ Passed to `display-buffer', which see."
   (interactive (hpg/interactive-args))
   (h/peer-graph topic hyperdrive sources-max-hops blockers-max-hops)
   (transient-setup 'hyperdrive-peer-graph-menu nil nil :scope hyperdrive))
-
-(defun hpg/load ()
-  "Load `hpg/relations' and redisplay graph."
-  ;; TODO: Refill name and color metadata.
-  ;; TODO: If called in rapid succession, stop the requests from the first call.
-  (setf hpg/relations
-        (hpg/relations
-         (h/public-key hpg/root-hyperdrive)
-         (or hpg/topic hpg/default-topic)
-         :sources-max-hops hpg/sources-max-hops
-         :blockers-max-hops hpg/blockers-max-hops
-         :finally
-         (lambda (relations)
-           (setf hpg/relations relations)
-           (h/fill-metadata-all
-            (cons hpg/root-hyperdrive
-                  (mapcar #'h/url-hyperdrive (hash-table-keys hpg/relations)))
-            :finally (lambda ()
-                       (hpg/display-graph)
-                       (hpg/refresh-menu))))))
-  (hpg/display-loading-buffer))
 
 (transient-define-suffix hpg/set-root-hyperdrive ()
   :transient t
@@ -318,24 +429,6 @@ Passed to `display-buffer', which see."
   :transient t
   (interactive)
   (hpg/load))
-
-(defun hpg/display-loading-buffer ()
-  "Open loading buffer for hyperdrive peer graph."
-  (with-current-buffer (get-buffer-create hpg/buffer-name)
-    (with-silent-modifications
-      (erase-buffer)
-      (insert "Loading hyperdrive peer graph data...")
-      (display-buffer (current-buffer) hpg/display-buffer-action))))
-
-(defun hpg/display-graph ()
-  "Open buffer displaying hyperdrive peer graph."
-  (with-current-buffer (get-buffer-create hpg/buffer-name)
-    (h/fons-view (hpg/filter hpg/relations)
-                 (h/public-key hpg/root-hyperdrive)
-                 :focus-ids (mapcar #'h/public-key hpg/paths-only-to)
-                 :label-fun #'hpg/label-fun)
-    (hpg/mode)
-    (pop-to-buffer (current-buffer) hpg/display-buffer-action)))
 
 (transient-define-suffix hpg/set-shortest-path-p ()
   :transient t
@@ -442,99 +535,10 @@ Only drives not in `hpg/paths-only-to' are offered for completion."
   (when-let ((buffer-window (get-buffer-window hpg/buffer-name)))
     (hpg/display-graph)))
 
-(cl-defun hpg/relations (root topic &key finally sources-max-hops blockers-max-hops)
-  "Load relations from ROOT about TOPIC and call FINALLY.
-FINALLY should be a function which accepts a single argument, a
-hash table of `fons-relation' structs keyed by public key.
-SOURCES-MAX-HOPS and BLOCKERS-MAX-HOPS are the maximum number of
-hops to traverse for sources and blockers, respectively."
-  (fons-relations
-   root :hops-fn #'hpg/hops-fn :topic "_blockers" :type 'blockers
-   :max-hops blockers-max-hops
-   :finally
-   (lambda (relations)
-     (fons-blocked
-      relations :blocked-fn #'hpg/blocked-fn :finally
-      (lambda (relations)
-        (fons-relations
-         root :relations relations :hops-fn #'hpg/hops-fn :topic topic
-         :type 'sources :max-hops sources-max-hops :finally
-         (lambda (relations)
-           (funcall finally relations))))))))
-
-(defun hpg/filter (relations)
-  "Return filtered RELATIONS."
-  ;; TODO: Make filters customizable
-  (unless (and hpg/show-sources-p hpg/show-blockers-p hpg/show-blocked-p hpg/show-all-blocked-p)
-    (cl-callf fons-filter-to-types relations
-      :sourcesp hpg/show-sources-p
-      :blockersp hpg/show-blockers-p
-      :blockedp hpg/show-blocked-p
-      :all-blocked-p (and hpg/show-blocked-p hpg/show-all-blocked-p)))
-  (when hpg/shortest-path-p
-    (cl-callf fons-filter-shortest-path relations))
-  ;; Apply `hpg/paths-only-to' last
-  (cl-callf2 fons-filter-paths-only-to
-      (mapcar #'h/public-key hpg/paths-only-to) relations)
-  relations)
-
 (defun hpg/refresh-menu ()
   "Refresh `hyperdrive-peer-graph-menu' if it's open."
   (when (transient-active-prefix 'hyperdrive-peer-graph-menu)
     (transient--refresh-transient)))
-
-(defvar hpg/topic-history nil
-  "Minibuffer history of `hyperdrive-peer-graph-read-topic'.")
-
-(defun hpg/read-topic ()
-  "Read topic string or nil with blank string."
-  (let ((topic (read-string "Topic (leave blank for default topic): "
-                            nil hpg/topic-history)))
-    (and (not (string-blank-p topic))
-         topic)))
-
-(cl-defun hpg/context-topic (&key force-prompt)
-  "Return `hyperdrive-peer-graph-topic'.
-With FORCE-PROMPT, or interactively with universal prefix
-argument \\[universal-argument], always prompt.
-Blank string defaults to `hyperdrive-peer-graph-default-topic'."
-  (if force-prompt (hpg/read-topic) hpg/topic))
-
-(cl-defun hpg/context-root-hyperdrive (&key force-prompt)
-  "Return `hyperdrive-peer-graph-root-hyperdrive' or prompt for drive.
-With FORCE-PROMPT, or interactively with universal prefix
-argument \\[universal-argument], always prompt."
-  (if (and hpg/root-hyperdrive (not force-prompt))
-      hpg/root-hyperdrive
-    (h/read-hyperdrive :default (or (and h/current-entry
-                                         (he/hyperdrive h/current-entry))
-                                    hpg/root-hyperdrive))))
-
-(defvar hpg/max-hops-history nil
-  "Minibuffer history of `hyperdrive-peer-graph-read-max-hops'.")
-
-(defun hpg/read-max-hops (type)
-  "Read max hops for \\+`sources' or \\+`blockers' TYPE."
-  (let ((default (pcase type
-                   ('sources hpg/sources-max-hops)
-                   ('blockers hpg/blockers-max-hops))))
-    (read-number (format "Max hops for %s" type) default
-                 hpg/max-hops-history)))
-
-(defun hpg/context-max-hops (type &key force-prompt)
-  "Return `hyperdrive-peer-graph-sources-max-hops' or prompt.
-With FORCE-PROMPT, or interactively with universal prefix
-argument \\[universal-argument], always prompt."
-  (if force-prompt
-      (hpg/read-max-hops type)
-    (pcase type
-      ('sources hpg/sources-max-hops)
-      ('blockers hpg/blockers-max-hops))))
-
-(defun hpg/loaded-relations ()
-  "Return `hyperdrive-peer-graph-relations' if loaded."
-  (and (not (processp hpg/relations))
-       hpg/relations))
 
 ;;; Footer:
 
