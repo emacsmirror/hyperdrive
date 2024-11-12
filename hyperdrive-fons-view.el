05dc0229680bde5ec3c8b13a9809d0f8a5d515e0
Joseph Turner <joseph@ushin.org>
2024-11-11 23:37:44 -0800

    Change: (hyperdrive-fons-view--format-graph) Remove nodesep

--- hyperdrive-fons-view.el
+++ hyperdrive-fons-view.el
@@ -261,7 +261,6 @@ graphviz string, and replaces it with the rendered output."
           (insert-vals "layout" layout
                        "bgcolor" (face-attribute 'default :background)
                        "overlap" hyperdrive-fons-view-overlap
-                       "nodesep""1"
                        "ranksep""1"
                        "margin" "0"
                        "ratio" "fill"
